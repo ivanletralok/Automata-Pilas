62e9f289615a35cb0946b7f1e38254c8fc31d0bb
LuisGuerrero10 <luisabelg2010@hotmail.com>
2017-10-07 00:58:33 -0500

    Codigo organizado

--- Automata.cpp
+++ Automata.cpp
@@ -1,38 +1,74 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <stdlib.h>
+
 
 using namespace std;
+
 #include "librerias/clases.h"
 
-#define INICIAL '#'
-
-
-
-
 int main(){
-	Grafo g(30);
-	g.crearVertice("A",1);
-	g.crearVertice("B",2);
-	g.crearVertice("C",2);
-	g.crearVertice("D",2);
-	g.crearVertice("E",3);
-	g.crearVertice("F",3);
+	Automata a(20);
+	RecorrerAutomata *ra = new RecorrerAutomata();
+	condicion f;
 
-	formato f;
-	f.cinta = 'A';
-	f.desapila = "#";
-	f.apila = "ABA";
-	g.crearArco("A","E",f);
-
-	f.cinta = 'B';
-	g.crearArco("A","E",f);
-
-	f.apila = "BB";
-	f.desapila = "A";
-	g.crearArco("A","E",f);
-	g.imprimirVertices();
-
-
+	int opcion,tipo;
+	string o,d;
+	do{
+		system("CLS");
+		cout<<"\t\tAutomata de pila\n";
+		cout<<"\n1. Crear estado\n2. Crear condicion\n3. Ver estados\n4. Ver condiciones\n5. Procesar expresion\n6. Salir\n\nEscoja una opcion: ";
+		cin>>opcion;
+		switch(opcion){
+			case 1:
+				system("CLS");
+				cout<<"\t\tCrear estado\n\nNombre: ";
+				cin>>o;
+				cout<<"\n\nTipo\n1. Inicial\n2. Transicion\n3.Aceptacion\nEscoja una opcion: ";
+				cin>>tipo;
+				a.crearEstado(o,tipo);
+				system("pause>0");
+				break;
+			case 2:
+				system("CLS");
+				cout<<"\t\tCrear condicion\n\nEstado origen: ";
+				cin>>o;
+				cout<<"Estado destino: ";
+				cin>>d;
+				cout<<"\n\nRestricciones\n1. Caracter leido de la cinta: ";
+				cin>>f.cinta;
+				cout<<"Desapila: ";
+				cin>>f.desapila;
+				cout<<"Apila: ";
+				cin>>f.apila;
+				a.crearCondicion(o,d,f);
+				system("pause>0");
+				break;
+			case 3:
+				system("CLS");
+				cout<<"\t\tVer estados\n\n";
+				a.imprimirEstados();
+				system("pause>0");
+				break;
+			case 4:
+				system("CLS");
+				cout<<"\t\tVer condiciones\n\n";
+				a.imprimirCondiciones();
+				system("pause>0");
+				break;
+			case 5:
+				system("CLS");
+				cout<<"\t\tProcesar expresion\n\n";
+				cout<<(ra->procesarExpresion(a) ? "\nExpresion aceptada" : "\nExpresion no aceptada");
+				system("pause>0");
+				break;
+			case 6:
+				cout<<"Fin del programa!";
+				break;
+			default:
+				cout<<"Opcion invalida intente nuevamente... somalparido: ";
+		}
+	}while(opcion != 6);
 
 }
