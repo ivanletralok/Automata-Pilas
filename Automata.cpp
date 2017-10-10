8cf5fdfd1e2cd3bebcfeb72692ae1883df3eb017
ivanletralok <31629155+ivanletralok@users.noreply.github.com>
2017-10-10 01:44:53 -0500

    Update Automata.cpp
    actualizando main

--- Automata.cpp
+++ Automata.cpp
@@ -16,22 +16,18 @@ int main(){
 	int opcion,tipo;
 	string o,d;
 	do{
-		system("CLS");
-		cout<<"\t\tAutomata de pila\n";
-		cout<<"\n1. Crear estado\n2. Crear condicion\n3. Ver estados\n4. Ver condiciones\n5. Procesar expresion\n6. Salir\n\nEscoja una opcion: ";
-		cin>>opcion;
 		switch(opcion){
-			case 1:
-				system("CLS");
+				case 1:
+           		        system("clear");
 				cout<<"\t\tCrear estado\n\nNombre: ";
 				cin>>o;
 				cout<<"\n\nTipo\n1. Inicial\n2. Transicion\n3.Aceptacion\nEscoja una opcion: ";
 				cin>>tipo;
 				a.crearEstado(o,tipo);
-				system("pause>0");
+             			   system("pause>0");
 				break;
-			case 2:
-				system("CLS");
+				case 2:
+                		system("clear");
 				cout<<"\t\tCrear condicion\n\nEstado origen: ";
 				cin>>o;
 				cout<<"Estado destino: ";
@@ -43,32 +39,32 @@ int main(){
 				cout<<"Apila: ";
 				cin>>f.apila;
 				a.crearCondicion(o,d,f);
-				system("pause>0");
+             		        system("pause>0");
 				break;
-			case 3:
-				system("CLS");
+				case 3:
+				system("clear");
 				cout<<"\t\tVer estados\n\n";
 				a.imprimirEstados();
 				system("pause>0");
 				break;
-			case 4:
-				system("CLS");
+				case 4:
+				system("clear");
 				cout<<"\t\tVer condiciones\n\n";
 				a.imprimirCondiciones();
 				system("pause>0");
 				break;
-			case 5:
-				system("CLS");
+				case 5:
+				system("clear");
 				cout<<"\t\tProcesar expresion\n\n";
 				cout<<(ra->procesarExpresion(a) ? "\nExpresion aceptada" : "\nExpresion no aceptada");
 				system("pause>0");
 				break;
-			case 6:
+				case 6:
 				cout<<"Fin del programa!";
 				break;
-			default:
-				cout<<"Opcion invalida intente nuevamente... somalparido: ";
+				default:
+				cout<<"Opcion invalida intente nuevamente...";
 		}
 	}while(opcion != 6);
-
+		
 }
